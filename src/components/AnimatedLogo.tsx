615c0f2aa26fab6d7a32ad624a9addeb928c7a3e
herfost <133514023+sbadung@users.noreply.github.com>
2023-08-05 01:38:24 +0200

    components refactor

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -4,15 +4,24 @@ interface AnimatedLogoProps {
   src: string;
 }
 
-export default function AnimatedLogo({ src }: AnimatedLogoProps) {
+const AnimatedLogo: React.FC<AnimatedLogoProps> = ({ src }: AnimatedLogoProps) => {
+  const animation = {
+
+    scale: [0, 1],
+    rotate: [-180, 0],
+  };
+
+  const transition = {
+    ease: "easeInOut",
+    duration: 1.5,
+    delay: 0.4,
+  };
+
   return (
-    <motion.div
-      whileHover={{ scale: 0.85 }}
-      whileTap={{ scale: 1.0 }}
-      drag="x"
-      dragConstraints={{ left: -100, right: 100 }} 
-    >
-      <img src={src} alt="" />
-    </motion.div>
+    <motion.div animate={animation} transition={transition} >
+      <img src={src} alt="logo" width={96} height={96} />
+    </ motion.div>
   );
 }
+
+export default AnimatedLogo;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,23 @@
 import Spline from '@splinetool/react-spline';
+import { motion } from "framer-motion";
 
-export default function Hero() {
+
+const Hero: React.FC = () => {
+  const initial = { opacity: 0, y: -180 };
+  const animate = { opacity: 1, y: 0 };
+  const transition = {
+    ease: "easeInOut",
+    duration: 1.5,
+    delay: 3,
+  };
+
   return (
-    <div className='w-auto h-auto'>
-      <Spline
-        scene="https://prod.spline.design/8k12xcBedad7ccv2/scene.splinecode" />
+    <div className="w-2/4 m-auto border-white border-solid border-2">
+      <motion.div initial={initial} animate={animate} transition={transition}>
+        <Spline scene="https://prod.spline.design/TM1DIoFJQ9sfzrEp/scene.splinecode" />
+      </motion.div>
     </div>
   );
 }
+
+export default Hero;
