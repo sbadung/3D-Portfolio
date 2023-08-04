6d00feae43aad1fee239414054f437a3e4d19fa9
herfost <133514023+sbadung@users.noreply.github.com>
2023-08-04 12:18:26 +0200

    added hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Hero() {
+  return (
+    <div className='w-auto h-auto'>
+      <Spline
+        scene="https://prod.spline.design/8k12xcBedad7ccv2/scene.splinecode" />
+    </div>
+  );
+}
