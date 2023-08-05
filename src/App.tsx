a5bc5a243e845f1a561c3722b7b69f69872309bf
herfost <133514023+sbadung@users.noreply.github.com>
2023-08-05 12:18:34 +0200

    added loader logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
+// Hooks
+import { useEffect, useState } from "react";
+
 import "./App.css"
+
+// Components
 import Hero from "./components/Hero";
 import Navbar from './components/Navbar';
+import Loader from "./components/Loader";
 
 
-export default function App() {
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadingDuration = 2000;
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, loadingDuration);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
   return (
     <>
-      <Navbar />
-      <Hero />
+      {loading
+        ? <Loader animationDuration={2} />
+        : <>
+          <Navbar />
+          <Hero />
+        </>
+      }
     </>
   );
 }
 
 
+export default App;
