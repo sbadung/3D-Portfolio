917f244cb2699422d952cf02d152e7005f19a82d
herfost <133514023+sbadung@users.noreply.github.com>
2023-08-05 12:17:22 +0200

    transition and size changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,11 +8,11 @@ const Hero: React.FC = () => {
   const transition = {
     ease: "easeInOut",
     duration: 1.5,
-    delay: 3,
+    delay: 0.6,
   };
-
+  
   return (
-    <div className="w-2/4 m-auto border-white border-solid border-2">
+    <div className="w-3/4 m-auto">
       <motion.div initial={initial} animate={animate} transition={transition}>
         <Spline scene="https://prod.spline.design/TM1DIoFJQ9sfzrEp/scene.splinecode" />
       </motion.div>
