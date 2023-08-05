6681423a67e4bce1ee2a8dd881b03bde1e14fc99
herfost <133514023+sbadung@users.noreply.github.com>
2023-08-05 12:16:42 +0200

    extended animations

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -2,24 +2,30 @@ import { motion } from "framer-motion"
 
 interface AnimatedLogoProps {
   src: string;
+  width: number;
+  height: number;
 }
 
-const AnimatedLogo: React.FC<AnimatedLogoProps> = ({ src }: AnimatedLogoProps) => {
-  const animation = {
-
+const AnimatedLogo: React.FC<AnimatedLogoProps> = ({ src, width, height }: AnimatedLogoProps) => {
+  const animate = {
     scale: [0, 1],
     rotate: [-180, 0],
   };
 
   const transition = {
     ease: "easeInOut",
-    duration: 1.5,
-    delay: 0.4,
+    duration: 1.5
   };
 
+  const whileHover = {
+    scale: [1, 0.25, 1],
+    rotate: [0, -360],
+    ease: "easeInOut",
+  }
+
   return (
-    <motion.div animate={animation} transition={transition} >
-      <img src={src} alt="logo" width={96} height={96} />
+    <motion.div animate={animate} transition={transition} whileHover={whileHover}>
+      <img src={src} alt="logo" width={width} height={height} />
     </ motion.div>
   );
 }
